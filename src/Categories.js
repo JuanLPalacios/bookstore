3be7a5dff6168c8d14710565f259f7cd4570f09d
JuanLP <juan.luis.palacios.p@gmail.com>
2022-04-01 13:21:35 -0500

    Add display message in Categories component

--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,8 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkCargory } from './redux/categories/categories';
 
 export default function Categories() {
+  const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
-  return <><button type="button" onClick={dispatch(checkCargory())}>Check status</button></>;
+  return <><button type="button" onClick={() => dispatch(checkCargory())}>{typeof categories === 'string' ? categories : 'Check status'}</button></>;
 }
